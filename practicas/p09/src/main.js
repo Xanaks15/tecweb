90f5bdf5df3bad908f8e624ffd5a25552e42603f
Xanaks15 <josue.torresz@alumno.buap.mx>
2024-10-09 22:21:07 -0600

    Validacion de Modelo, precio y detalles

--- practicas/p09/src/main.js
+++ practicas/p09/src/main.js
@@ -8,8 +8,9 @@ function checkName(campo){
     }
     else{
         if(nombre.length > 100){
-        alert('El campo debe tener maximo 100 caracteres');
-        return false;
+            alert('El campo debe tener maximo 100 caracteres');
+            campo.value = '';
+            return false;
         }
     return true;
     }
@@ -47,6 +48,42 @@ function checkModel(campo){
         }
     }
     else{
-        if(modelo){
-        }  
+        /*Validad que el campo sea alfanumerico y menor a 25 caracateres*/
+        if(!/^[a-zA-Z0-9]+$/.test(modelo) || modelo.length > 25){
+            alert('El campo debe ser alfanumerico y menor a 25 caracteres');
+            campo.value = '';
+            return false;
+        }
+    }
+    return true; 
+}
+
+function checkPrice(campo){
+    precio = campo.value;
+    if(precio.length == 0){
+        while(precio.length == 0){
+            alert('Campo requerido');
+            return false;
+        }
+    }
+    else{
+        if(precio < 99.99){
+            alert('El campo debe ser mayor a $99.99');
+            campo.value = '';
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkDetalles(campo){
+    detalles = campo.value;
+    if(detalles.length > 0){
+        if(detalles.length > 250){
+            alert('El campo debe tener maximo 250 caracteres');
+            campo.value = '';
+            return false;
+        }
+    }
+    return true;
 }
