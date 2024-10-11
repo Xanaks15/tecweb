e91a44d9a0538180cf33e467a820b5c608890682
Xanaks15 <josue.torresz@alumno.buap.mx>
2024-10-10 20:21:06 -0600

    Correción de main.js y validación completa de formulario

--- practicas/p09/src/main.js
+++ practicas/p09/src/main.js
@@ -1,28 +1,24 @@
-function checkName(campo){
-    nombre = campo.value;
+function checkName(){
+    var nombre = document.getElementById('form-nombre').value;
     if(nombre.length == 0){
-        while(nombre.length ==  0){
-            alert('Campo requerido');
-            return false;
-        }
+        alert('Ingresa un nombre');
+        return false;
     }
     else{
         if(nombre.length > 100){
-            alert('El campo debe tener maximo 100 caracteres');
-            campo.value = '';
+            alert('El nombre debe tener maximo 100 caracteres');
+            document.getElementById('nombre').value = '';
             return false;
         }
     return true;
     }
 }
 
-function checkMarca(campo){
-    marca = campo.value;
+function checkMarca(){
+    var marca = document.getElementById('marca').value;
     if(marca.length == 0){
-        while(marca.length == 0){
         alert('Selecciona una marca');
         return false;
-        }
     } 
     else{
         switch(marca) {
@@ -32,107 +28,103 @@ function checkMarca(campo){
             case 'Sony':
             case 'Xiaomi':
                 return true;
+                val[2]=0
             default:
-                alert('Marca no valida');
+                checkMarca();
                 return false;
         }
     }
 }
 
-function checkModel(campo){
-    modelo = campo.value;
+function checkModel(){
+    var modelo = document.getElementById('modelo').value;
     if(modelo.length == 0){
-        while(modelo.length == 0){
-            alert('Campo requerido');
-            return false;
-        }
+        alert('Ingresa un modelo');
+        return false;
     }
     else{
         /*Validad que el campo sea alfanumerico y menor a 25 caracateres*/
         if(!/^[a-zA-Z0-9]+$/.test(modelo) || modelo.length > 25){
-            alert('El campo debe ser alfanumerico y menor a 25 caracteres');
-            campo.value = '';
+            alert('El modelo debe ser alfanumerico y menor a 25 caracteres');
+            document.getElementById('modelo').value = '';
             return false;
         }
     }
     return true; 
 }
 
-function checkPrice(campo){
-    precio = campo.value;
+function checkPrice(){
+    var precio = document.getElementById('precio').value;
     if(precio.length == 0){
-        while(precio.length == 0){
-            alert('Campo requerido');
-            return false;
-        }
+        alert('Ingresa el precio');
+        return false;
+
     }
     else{
         if(precio < 99.99){
-            alert('El campo debe ser mayor a $99.99');
-            campo.value = '';
+            alert('El precio debe ser mayor a $99.99');
+            document.getElementById('precio').value = '';
             return false;
         }
     }
     return true;
 }
 
-function checkDetalles(campo){
-    detalles = campo.value;
+function checkDetalles(){
+    var detalles = document.getElementById('detalles').value;
     if(detalles.length > 0){
         if(detalles.length > 250){
-            alert('El campo debe tener maximo 250 caracteres');
-            campo.value = '';
+            alert('Los etalles deben tener maximo 250 caracteres');
+            document.getElementById('detalles').value = '';
             return false;
         }
     }
     return true;
 }
 
-function checkUnidades(campo){
-    unidades = campo.value;
+function checkUnidades(){
+    var unidades = document.getElementById('unidades').value;
     if(unidades.length == 0){
         while(unidades.length == 0){
-            alert('Campo requerido');
+            alert('Ingresa la cantidad de unidades');
             return false;
         }
     }
     else{
         if(unidades < 0){
             alert('Cantidad minima 0');
-            campo.value = '';
+            document.getElementById('unidades').value = '';
             return false;
         }
     }
     return true;
 }
 
-function checkImg(campo){
-    imagen = campo.value;
+function checkImg(){
+    var imagen = document.getElementById('imagen').value;
     if(imagen.length == 0){
-        campo.value = 'img/pre.png';
+        document.getElementById('imagen').value = 'img/pre.png';
         return true;
+        val[7]=0
     }
     else{
         if(imagen.length > 0){
             if(!/^img\/\w+\.(jpg|png)$/.test(imagen)){
                 alert('URL no valida. Debe ser en el formato img/nombre.(jpg o png)');
-                campo.value = '';
+                document.getElementById('imagen').value = '';
                 return false;
             }
         }
     }
+    return true;
 }
 
 function checkForm(){
-    var form = document.getElementById('form');
-    var nombre = document.getElementById('nombre');
-    var marca = document.getElementById('marca');
-    var modelo = document.getElementById('modelo');
-    var precio = document.getElementById('precio');
-    var detalles = document.getElementById('detalles');
-    var unidades = document.getElementById('unidades');
-
-    if(checkName(nombre) && checkMarca(marca) && checkModel(modelo) && checkPrice(precio) && checkDetalles(detalles) && checkUnidades(unidades)){
-        form.submit();
+    var check = [checkName(),checkMarca(),checkModel(),checkPrice(),checkDetalles(),checkUnidades(),checkImg()];
+    if(check.includes(false)){
+        return false;
+    }
+    else{
+        return true;
     }
 }
